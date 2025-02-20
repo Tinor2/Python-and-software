bc0882736f95698f99882a5a6d3ac788969aabd4
Tinor2 <ironitbhandari@gmail.com>
2025-02-21 08:03:21 +1100

    Continued updating, fixing structure and debugging main.py file. A very basic version of hangman is ready

--- Software SCHOOL/Stream 2/Hangman Stream 2/main.py
+++ Software SCHOOL/Stream 2/Hangman Stream 2/main.py
@@ -1,39 +1,62 @@
-from formattingText import colors
+import utils
 class Hangman:
-    def __init__(self, word, totalLives) -> None:
+    def __init__(self, word:str, totalLives) -> None:
         self.word = word
         self.guesses = set()
         self.lives = totalLives
         self.progress = 0
     def checkForEnd(self):
+        print(f"word: {sorted(set(self.word))}, guesses used: {sorted(self.guesses)}")
         if self.lives <= 0:
             return True, "L" #loss
-        elif list(self.guesses).sort() == list(self.word).sort: #note: Not scalable
+        elif  set(sorted(self.word)) <= set(sorted(self.guesses)): #note: Not scalable
+            print("Win cond")
             return True,"W"
         else:
             return False,"-"
-    def processGuess(self, guess):
+    def processGuess(self, guess:str):
         if len(guess) == 1:
+            self.guesses.add(guess)
             if guess in self.guesses:
                 self.lives -= 1
+                print(f"Total lives left: {self.lives}")
                 return "Already used letter, loose a life"
             elif guess not in self.word:
                 self.lives -= 1
+                print(f"Total lives left: {self.lives}")
                 return "Wrong choice, loose a life"    
-            self.guesses.add(guess)
         else:
             if self.word == guess:
-                for letter in guess: self.guesses.add(guess)
-                if self.checkForEnd() != (True,"W"):
-                    self.lives -= 1
-                    return "Wrong choice, loose a life"  
+                for letter in guess: self.guesses.add(letter)
+                return "Correct, Win condition"
+            else:
+                self.lives -= 1
+                print(f"Total lives left: {self.lives}")
+                return "Wrong choice, loose a life" 
         return "Correct Choice!" #Before the next guess is called, checkForEnd() should be called again, and we should check if the game should end or not
-    def renderWord(self, iterable):
+    def renderWord(self):
         display = ""
-        for char in iterable: # could be simplified to collection
+        for char in self.word: # could be simplified to collection
             if char in self.guesses:
-                display += f" % "%char
+                display += f" {char} "
             else:
                 display += " _ "
         return display
-    
+
+currentGame = Hangman("car", 5)
+while True:
+    print(currentGame.processGuess(input("guess: ")))
+    isEnd = currentGame.checkForEnd()
+    print(isEnd)
+    if isEnd[0] == True:
+        if isEnd[1] == "W":
+            print("You won! ")
+        elif isEnd[1] == "L":
+            print("You Lost! ")
+        break
+    print(currentGame.renderWord())
+'''
+Current Bugs:
+currentGame.checkForEnd() does not remotely work
+typing in "aa" for the target word "car" makes the game think tat
+'''
