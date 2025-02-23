b65ecf0209a6be8827314f2688c7edf125c0831b
Tinor2 <ironitbhandari@gmail.com>
2025-02-23 17:41:52 +1100

    Finished a rudimentry, OOP based hangman

--- Software SCHOOL/Stream 2/Hangman Stream 2/main.py
+++ Software SCHOOL/Stream 2/Hangman Stream 2/main.py
@@ -10,18 +10,18 @@ class Hangman:
         if self.lives <= 0:
             return True, "L" #loss
         elif  set(sorted(self.word)) <= set(sorted(self.guesses)): #note: Not scalable
-            print("Win cond")
             return True,"W"
         else:
             return False,"-"
     def processGuess(self, guess:str):
         if len(guess) == 1:
-            self.guesses.add(guess)
-            if guess in self.guesses:
+            if guess in self.guesses: # is a in {...}
+                self.guesses.add(guess) 
                 self.lives -= 1
                 print(f"Total lives left: {self.lives}")
                 return "Already used letter, loose a life"
             elif guess not in self.word:
+                self.guesses.add(guess) 
                 self.lives -= 1
                 print(f"Total lives left: {self.lives}")
                 return "Wrong choice, loose a life"    
@@ -55,8 +55,4 @@ while True:
             print("You Lost! ")
         break
     print(currentGame.renderWord())
-'''
-Current Bugs:
-currentGame.checkForEnd() does not remotely work
-typing in "aa" for the target word "car" makes the game think tat
-'''
+
