27d8d760809defb48e1b186ad73927a6614a764a
Tinor2 <ironitbhandari@gmail.com>
2025-02-27 07:14:12 +1100

    added comments for the diff erent variables each function in the class uses, as well cleaning up the code

--- Software SCHOOL/Stream 2/Hangman Stream 2/main.py
+++ Software SCHOOL/Stream 2/Hangman Stream 2/main.py
@@ -7,29 +7,30 @@ class Hangman:
         self.guesses = set()
         self.points = totalPoints
         self.progress = 0
-    def checkForEnd(self):
-        print(f"word: {sorted(set(self.word))}, guesses used: {sorted(self.guesses)}")
+    def checkForEnd(self): #uses self.word, self.guesses, self.points
+        # print(f"word: {sorted(set(self.word))}, guesses used: {sorted(self.guesses)}")
         if self.points <= 0:
             return True, "L" #loss
         elif  set(sorted(self.word)) <= set(sorted(self.guesses)): #note: Not scalable
             return True,"W"
         else:
             return False,"-"
-    def processGuess(self, guess:str):
+    def processGuess(self, guess:str): #uses guess, self.guesses, self.word, self.points
         if len(guess) == 1:
             if guess in self.guesses: # is a in {...}
                 self.guesses.add(guess) 
                 self.points -= 10
-                print(f"Total lives left: {self.points}")
+                print(f"Total points: {self.points}")
                 return "Already used letter, loose a life"
             elif guess not in self.word:
                 self.guesses.add(guess) 
                 self.points -= 10
-                print(f"Total lives left: {self.points}")
+                print(f"Total points: {self.points}")
                 return "Wrong choice, loose a life"    
             else:
                 self.guesses.add(guess)
                 self.points += 10
+                print(f"Total points: {self.points}")
                 return "Correct Choice!"
         else:
             if self.word == guess:
@@ -39,8 +40,8 @@ class Hangman:
             else:
                 self.points -= 10
                 print(f"Total lives left: {self.points}")
-                return "Wrong choice, loose a life" 
-    def renderWord(self):
+                return "Wrong choice, loose a life"
+    def renderWord(self): # uses self.word, self.guesses
         display = ""
         for char in self.word: # could be simplified to collection
             if char in self.guesses:
@@ -48,17 +49,16 @@ class Hangman:
             else:
                 display += " _ "
         display += "\n"
-        display += f"guesses used: {sorted(self.guesses)}"
+        display += f"guesses used: {' '.join(map(str, sorted(self.guesses)))}"
         return display
 
-def load_game_data():
+def load_game_data(): #loads and acceses database
     with open('/Users/ronitbhandari/Desktop/Projects/Software SCHOOL/Stream 2/Hangman Stream 2/data.json', 'r') as file:
         game_data = json.load(file)
     words = game_data['words']
     difficulties = game_data['difficulties']
     return (words, difficulties)
-
-def chooseDif():
+def chooseDif(): # uses loaded data
     allWords, difficultyOptions= load_game_data()
     options = []
     for difficulty in difficultyOptions.values():
@@ -66,7 +66,6 @@ def chooseDif():
     while True:
         userChoice = input("Choose a difficulty: ").lower()
         if userChoice in options:
-            print("fds")
             if userChoice in difficultyOptions["easy"]["shortcuts"]:
                 finalDifficulty = "easy"
             elif userChoice in difficultyOptions["medium"]["shortcuts"]:
