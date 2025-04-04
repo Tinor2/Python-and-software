4ec21c476ac89d6a04048be1a460901af78134d5
Tinor2 <ironitbhandari@gmail.com>
2025-04-04 13:50:22 +1100

    getting rid of unneccesary files for simulations

--- STEM work/STEM worka/microbitProj/main.py
+++
@@ -1,4 +0,0 @@
-expoSetting = 2 #F: This will always return 2, does not change
-expoFactor = 45 * 45 / (45 - 45 / expoSetting) #F: This will always return 90, and it does not eery change
-
-print(expoFactor)
