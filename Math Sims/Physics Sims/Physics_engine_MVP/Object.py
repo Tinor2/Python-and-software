f1a9ce42b0ab5e0ca310c1ff53f3d4c8db6bd1f4
Tinor2 <ironitbhandari@gmail.com>
2025-04-07 14:18:02 +1000

    added collision correction

--- Math Sims/Physics Sims/Physics_engine_MVP/Object.py
+++ Math Sims/Physics Sims/Physics_engine_MVP/Object.py
@@ -1,21 +1,29 @@
 import time
+from Object_dependencies import Hitbox
 TOTAL_LAYERS = 5
 TIME_STEP = 1/60  # Time step in seconds (1/FPS)
 
 
 class Object:
-    def __init__(self, mass, position, velocity):
+    def __init__(self, mass, position:list[int], velocity):
         self.mass = mass
         self.position = position
         self.velocity = velocity
         self.force = [0, 0]
         self.acceleration = [self.force[0]/mass, self.force[1]/mass]
+        self.col = None
+    def add_collider(self, w,l,layer):
+        self.col = Hitbox(self,w,l,layer)
+
     def apply_force(self, force, dt):
         self.force += force
     def update_position(self, dt):
         self.velocity[0] += self.acceleration[0] * dt
         self.velocity[1] += self.acceleration[1] * dt
-
-
+        self.position[0] += self.velocity[0] * dt
+        self.position[1] += self.velocity[1] * dt
+        if self.col and self.col.check_collision():
+            offset = self.col.check_collision()
+            # self.position += offset
 def update(self, dt = TIME_STEP):
     time.sleep(dt)
--- Math Sims/Physics Sims/Physics_engine_MVP/Object_dependencies.py
+++ Math Sims/Physics Sims/Physics_engine_MVP/Object_dependencies.py
@@ -4,7 +4,7 @@ class Hitbox:
     all_hitboxes = {}
     for layer in range(1, TOTAL_LAYERS + 1):
         all_hitboxes[layer] = []
-    def __init__(self, object:Object,width, height, layer, perfect_elastic = False):
+    def __init__(self, object:Object,width, height, layer, perfect_elastic = True):
         if layer not in Hitbox.all_hitboxes.keys():
             raise ValueError(f"Layer {layer} does not exist.")
         Hitbox.all_hitboxes[layer].append(self)
@@ -21,5 +21,32 @@ class Hitbox:
                self.object.position[0] + self.width > hitbox.object.position[0] and \
                self.object.position[1] < hitbox.object.position[1] + hitbox.height and \
                self.object.position[1] + self.height > hitbox.object.position[1]:
-                return True
-        return False
+                return hitbox
+        return None
+    def correct_collision(self) -> list[int]:
+        colliding_hitbox = self.check_collision()
+        if not colliding_hitbox:
+            return [0, 0]  # No collision, no correction needed
+        
+        # Calculate overlap in both axes
+        x_overlap = min(
+            abs(self.object.position[0] + self.width - colliding_hitbox.object.position[0]),  # Right edge overlap
+            abs(colliding_hitbox.object.position[0] + colliding_hitbox.width - self.object.position[0])  # Left edge overlap
+        )
+        
+        y_overlap = min(
+            abs(self.object.position[1] + self.height - colliding_hitbox.object.position[1]),  # Bottom edge overlap
+            abs(colliding_hitbox.object.position[1] + colliding_hitbox.height - self.object.position[1])  # Top edge overlap
+        )
+        
+        # Determine which direction requires less movement
+        if x_overlap < y_overlap:
+            # Correct horizontally
+            if self.object.position[0] < colliding_hitbox.object.position[0]:
+                return [-x_overlap, 0]  # Move left
+            return [x_overlap, 0]  # Move right
+        else:
+            # Correct vertically
+            if self.object.position[1] < colliding_hitbox.object.position[1]:
+                return [0, -y_overlap]  # Move up
+            return [0, y_overlap]  # Move down
