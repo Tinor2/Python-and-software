2794652d34cc90659ed53e249a0f42793378cab9
Tinor2 <ironitbhandari@gmail.com>
2025-04-08 10:16:44 +1000

    Added change in velocity, based on collision

--- Math Sims/Physics Sims/Physics_engine_MVP/Object.py
+++ Math Sims/Physics Sims/Physics_engine_MVP/Object.py
@@ -22,8 +22,21 @@ class Object:
         self.velocity[1] += self.acceleration[1] * dt
         self.position[0] += self.velocity[0] * dt
         self.position[1] += self.velocity[1] * dt
-        if self.col and self.col.check_collision():
-            offset = self.col.check_collision()
-            # self.position += offset
+        
+        if self.col:
+            colliding_hitbox = self.col.check_collision()
+            if colliding_hitbox:
+                offset = self.col.correct_collision()
+                if offset:
+                    self.position = [self.position[0] + offset[0], self.position[1] + offset[1]]
+                # Handle collision
+                if self.col.perfect_elastic:
+                    # Perfectly elastic collision
+                    self.velocity[0] = -self.velocity[0]
+                    self.velocity[1] = -self.velocity[1]
+                else:
+                    # Inelastic collision (simple bounce)
+                    self.velocity[0] *= -0.5
+                    self.velocity[1] *= -0.5
 def update(self, dt = TIME_STEP):
     time.sleep(dt)
