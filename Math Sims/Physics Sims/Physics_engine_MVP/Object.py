5d04aea74ad7a022180105913c864c8a89d9ecd2
Tinor2 <ironitbhandari@gmail.com>
2025-04-12 14:39:16 +1000

    started creating a rednerer. Currently the renderer has not been reviewed at all, and I may end up completely overhauling the current system

--- Math Sims/Physics Sims/Physics_engine_MVP/Object.py
+++ Math Sims/Physics Sims/Physics_engine_MVP/Object.py
@@ -1,21 +1,22 @@
 import time
 from Object_dependencies import Hitbox
+from Physics_Renderer import GridRenderer
 TOTAL_LAYERS = 5
 TIME_STEP = 1/60  # Time step in seconds (1/FPS)
 
 
 class Object:
-    def __init__(self, mass, position:list[int], velocity):
+    def __init__(self, mass, position:list[int], velocity, initial_force = [0, 0]):
         self.mass = mass
         self.position = position
         self.velocity = velocity
-        self.force = [0, 0]
+        self.force = initial_force
         self.acceleration = [self.force[0]/mass, self.force[1]/mass]
         self.col = None
     def add_collider(self, w,l,layer):
         self.col = Hitbox(self,w,l,layer)
 
-    def apply_force(self, force, dt):
+    def apply_force(self, force:list[int]):
         self.force += force
     def update_position(self, dt):
         self.velocity[0] += self.acceleration[0] * dt
@@ -38,5 +39,10 @@ class Object:
                     # Inelastic collision (simple bounce)
                     self.velocity[0] *= -0.5
                     self.velocity[1] *= -0.5
+def start():
+    new_object = Object(mass= 1, position= [0, 0], velocity= [0, 0], initial_force= [0, 0])
+    new_object.col = Hitbox(new_object, 10, 10, 1)
+    new_object.apply_force([0,5])
 def update(self, dt = TIME_STEP):
+    self.update_position(dt)
     time.sleep(dt)
